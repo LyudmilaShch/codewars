7aa275c4feb967ab30d40fa7dfbf86e7723b0df0
LyudmilaShch <111302180+LyudmilaShch@users.noreply.github.com>
2022-08-23 17:48:07 +0300

    новые задачи

---
+++ Youonlyneedone.js
@@ -0,0 +1,22 @@
+//???? 
+function check(a, x) {
+    if a.includes(x){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  //You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+//Array can contain numbers or strings. X can be either.
+//Return true if the array contains the value, false if not.
+
+
+function check(a, x) {
+    for (var i = 0; i < a.length; i++) {
+         if (a[i] === x) {
+             return true;
+         }
+     }
+     return false;
+ }
