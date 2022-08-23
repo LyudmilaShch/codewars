2fe0f8d09197e5ec4cde50a54afcfe25917b2a2b
LyudmilaShch <111302180+LyudmilaShch@users.noreply.github.com>
2022-08-24 00:58:29 +0300

    Обновлен Smallenough.js

--- Smallenough.js
+++ Smallenough.js
@@ -1,12 +1,14 @@
-?????
 function smallEnough(a, limit){
-    for (let i = 0; i < a.length; i++) {
-      if (a[i] <= limit){
-        return true;
-      }        
-    }
+    
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] > limit){ 
       return false;
+      break
+    }
   }
+  return true;
+}
+console.log(smallEnough([101, 45, 12, 105, 99, 105], 107));
 
 //You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
 //You can assume all values in the array are numbers.
