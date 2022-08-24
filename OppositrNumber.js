f534f8a44b37ac2fd50d9254e1ad8502843f1012
LyudmilaShch <111302180+LyudmilaShch@users.noreply.github.com>
2022-08-24 17:33:03 +0300

    Добавлен Oppositr number

---
+++ OppositrNumber.js
@@ -0,0 +1,13 @@
+function opposite(number) {
+    let result = - number;
+    return result;
+  }
+  console.log(opposite(5))
+
+/*Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34*/
