7bca6362d5c420fb6d5f8189981657b46cb4ece2
LyudmilaShch <111302180+LyudmilaShch@users.noreply.github.com>
2022-08-24 17:28:30 +0300

    Добавлен Четное или нечетное

---
+++ EvenOrOdd.js
@@ -0,0 +1,12 @@
+function even_or_odd(number) {
+    if (number % 2 === 0){
+           result = "Even";
+        } else {
+           result = "Odd";
+        }
+      return result;
+ }
+    console.log(even_or_odd(4))
+
+
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
