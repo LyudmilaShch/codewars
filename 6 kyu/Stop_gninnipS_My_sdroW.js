fd94a79944e449512e7f4ad0f0c040e877cca06a
Lyudmila Shchepina <lyudashchepina1999@gmail.com>
2022-10-08 14:32:19 +0300

    еще задачи

---
+++ 6 kyu/Stop_gninnipS_My_sdroW.js
@@ -0,0 +1,32 @@
+function spinWords(string){
+    var arr = string.split(' ');
+    let result = []
+    
+       for (var i = 0; i < arr.length; i++) {
+         if(arr[i].length >= 5){
+           result.push(arr[i].split("").reverse().join(""))
+         } else {
+             result.push(arr[i])
+         }
+       }
+       return result.join(' ');
+        }
+   
+   
+   console.log(spinWords("Hey fellow warriors"))
+
+   /*Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+Examples:
+
+spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+spinWords( "This is a test") => returns "This is a test" 
+spinWords( "This is another test" )=> returns "This is rehtona test"*/
+
+function spinWords(string){
+    return string.split(' ').map( w => 
+          (w.length >= 5 ? w.split("").reverse().join(""): w)).join(" ")
+         }
+    
+    
+    console.log(spinWords("Hey fellow warriors"))
